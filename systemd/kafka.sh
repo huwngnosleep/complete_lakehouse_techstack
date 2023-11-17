c9e242cfb4f699e463a51f6bb0fe0235f20fde65
huwngnosleep <hung.nt3@kiotviet.com>
2023-11-17 17:51:27 +0700

    improve kafka.sh systemd file

--- systemd/kafka.sh
+++ systemd/kafka.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+echo "START LOADING VARIABLE..."
+cat /etc/environment
+source /etc/environment
+
 echo "kafka.service: ## Starting ##" 
 
 while :
 do
-TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
-echo "try restarting kafka.service: timestamp ${TIMESTAMP}"
-bash /opt/kafka_2.13-3.6.0/bin/zookeeper-server-start.sh -daemon /opt/kafka_2.1>
-bash /opt/kafka_2.13-3.6.0/bin/kafka-server-start.sh -daemon /opt/kafka_2.13-3.>
-sleep 60
+    TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+    echo "try restarting kafka.service: timestamp ${TIMESTAMP}"
+
+    ${KAFKA_HOME}/bin/zookeeper-server-start.sh -daemon ${KAFKA_HOME}/config/zookeeper.properties
+    ${KAFKA_HOME}/bin/kafka-server-start.sh -daemon ${KAFKA_HOME}/config/server.properties
+    ${HADOOP_HOME}/sbin/start-dfs.sh
+
+    sleep 60
 done
