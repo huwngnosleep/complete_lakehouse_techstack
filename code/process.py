aead9706bcef4135a805f868eb60d876bf94b7cb
huwngnosleep <thanhhung8210@gmail.com>
2023-12-07 18:15:27 +0700

    add pyspark app

---
+++ code/process.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType
+
+# Define the Kafka topic to read from
+kafka_topic = "kafka-mysql-hung-test.test.inventory"
+ivy2_repository = "/home/spark/"
+# Define the schema for your data
+schema = StructType([
+    StructField("key", StringType(), True),
+    StructField("value", StringType(), True),
+])
+
+# Create a Spark session
+spark = SparkSession.builder \
+    .appName("KafkaStreamReader") \
+    .config("spark.jars.ivy", ivy2_repository) \
+    .getOrCreate()
+
+# Read data from Kafka
+df = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "172.21.0.15:9092") \
+    .option("subscribe", kafka_topic) \
+    .load()
