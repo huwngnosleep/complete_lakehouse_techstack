0f1b49753710d32db099d994b6afef32350ff463
huwngnosleep <thanhhung8210@gmail.com>
2023-12-12 17:57:10 +0700

    improvement

--- scripts/start_sparkthrift.sh
+++ scripts/start_sparkthrift.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+source ../.env
+echo SPARK MASTER $SPARK_MASTER_HOST:$SPARK_MASTER_PORT
 while true; do
-    bash $SPARK_HOME/sbin/start-thriftserver.sh --master spark://spark-master:7077 --properties-file $SPARK_HOME/conf/spark-thriftserver.properties
+    bash $SPARK_HOME/sbin/start-thriftserver.sh --master spark://$SPARK_MASTER_HOST:$SPARK_MASTER_PORT --properties-file $SPARK_HOME/conf/spark-thriftserver.properties
 
-    # Add a delay between retries to avoid excessive requests
     sleep 30
 done
