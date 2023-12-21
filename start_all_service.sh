940414fcf88b377054f5c78f833ffe06fc898b69
Nguyễn Thành Hưng - Falcon <hung.nt3@kiotviet.com>
2023-12-21 23:08:10 +0700

    refactor

---
+++ start_all_service.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Use the ls command to get a list of files in the current directory
+dirs=$(ls ./services)
+
+# Loop through each file in the list
+for sub_dir in $dirs; do
+    docker compose -f ./services/$sub_dir/docker-compose.yml up -d
+done
+
+# print all container and ips
+echo RUNNGING CONTAINERS:
+
+docker inspect -f '{{index (split .Name "/") 1}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq)
