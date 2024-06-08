0e2ea7e45f9e5ce79c14781579556cbe54b6e48f
huwngnosleep <thanhhung8210@gmail.com>
2024-06-08 21:23:04 +0700

    update sale pipeline

--- services/airflow/dags/migration_sale_data.py
+++ services/airflow/dags/migration_sale_data.py
@@ -3,17 +3,39 @@ from datetime import datetime
 from airflow import DAG
 import pandas
 import pymssql
+from tempfile import NamedTemporaryFile
+import pyhdfs
 DAG_ID = "migration_sale_data"
 with DAG(
     DAG_ID,
     schedule=None,
-    start_date=datetime(2021, 10, 1),
     catchup=False,
 ) as dag:
+    ls_raw_tables = [
+        "production.categories",
+        "production.brands",
+        "production.products",
+        "sales.customers",
+        "sales.stores",
+        "sales.staffs",
+        "sales.orders",
+        "sales.order_items",
+        "production.stocks",
+    ]
+    
     @dag.task(task_id="get_raw_data")
     def get_raw_data():
-        conn = pymssql.connect('mssql:1433', 'sa', 'root@@@123', "retail")
-        data = pandas.read_sql("SELECT * FROM sales.orders;", conn)
-        print("data", data)
-        conn.close()
+        mssql_conn = pymssql.connect('mssql:1433', 'sa', 'root@@@123', "retail")
+        hdfs_client = pyhdfs.HdfsClient(hosts='namenode:9870')
+        for table in ls_raw_tables:
+            raw_data = pandas.read_sql(f"SELECT * FROM {table};", mssql_conn)
+            temp_file = NamedTemporaryFile()
+            raw_data.to_parquet(temp_file.name)
+            hdfs_raw_dir = f"/sale_warehouse/raw/{table}/"
+            dest_file_name = f"{table}_01.snappy.parquet"
+            hdfs_client.delete(hdfs_raw_dir + dest_file_name)
+            if not hdfs_client.exists(hdfs_raw_dir):
+                hdfs_client.mkdirs(hdfs_raw_dir)
+            hdfs_client.create(hdfs_raw_dir + dest_file_name, temp_file.name)
+        mssql_conn.close()
     get_raw_data()
