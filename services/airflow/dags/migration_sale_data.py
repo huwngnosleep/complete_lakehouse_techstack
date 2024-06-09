806acb2796900e79ec8bd7ff0f86df5689614d83
huwngnosleep <thanhhung8210@gmail.com>
2024-06-09 12:34:40 +0700

    add dependencies for spark-thrift

--- services/airflow/dags/migration_sale_data.py
+++ services/airflow/dags/migration_sale_data.py
@@ -5,6 +5,7 @@ import pandas
 import pymssql
 from tempfile import NamedTemporaryFile
 import pyhdfs
+from pyhive import hive
 DAG_ID = "migration_sale_data"
 with DAG(
     DAG_ID,
@@ -22,7 +23,8 @@ with DAG(
         "sales.order_items",
         "production.stocks",
     ]
-    
+    base_path = '/sale_warehouse'
+    RAW = "/raw"
     @dag.task(task_id="get_raw_data")
     def get_raw_data():
         mssql_conn = pymssql.connect('mssql:1433', 'sa', 'root@@@123', "retail")
@@ -31,7 +33,7 @@ with DAG(
             raw_data = pandas.read_sql(f"SELECT * FROM {table};", mssql_conn)
             temp_file = NamedTemporaryFile()
             raw_data.to_parquet(temp_file.name)
-            hdfs_raw_dir = f"/sale_warehouse/raw/{table}/"
+            hdfs_raw_dir = f"{base_path}{RAW}/{table}/"
             dest_file_name = f"{table}_01.snappy.parquet"
             hdfs_client.delete(hdfs_raw_dir + dest_file_name)
             if not hdfs_client.exists(hdfs_raw_dir):
@@ -42,3 +44,18 @@ with DAG(
             )
         mssql_conn.close()
     get_raw_data()
+    
+    @dag.task(task_id="create_staging_tables")
+    def create_staging_tables():
+        cursor = hive.connect(host='spark-thriftserver', port=10000).cursor() 
+        for table in ls_raw_tables:
+            raw_table_name = f"spark_catalog.default.{table.replace(".", "_")}_raw"
+            cursor.execute(f"""
+                DROP TABLE IF EXISTS {raw_table_name}
+            """)
+            cursor.execute(f"""
+                CREATE TABLE {raw_table_name}
+                USING parquet
+                LOCATION '{base_path}{RAW}/{table}/*.snappy.parquet'
+            """)
+    create_staging_tables()
