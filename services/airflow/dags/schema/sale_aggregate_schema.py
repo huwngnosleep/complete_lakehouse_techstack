3d604574a4082b517d3d9be8bed0d596d43c25cb
huwngnosleep <thanhhung8210@gmail.com>
2024-06-23 18:20:45 +0700

    add clickhouse, change schema

--- services/airflow/dags/schema/sale_aggregate_schema.py
+++ services/airflow/dags/schema/sale_aggregate_schema.py
@@ -6,12 +6,20 @@ REVENUE_BY_TIME = {
         AS
         SELECT
         o.order_id, o.customer_id, o.store_id, o.staff_id, 
-        o.order_status, o.order_date,
+         CASE
+            WHEN o.order_status = 1 THEN 'Pending'
+            WHEN o.order_status = 2 THEN 'Processing'
+            WHEN o.order_status = 3 THEN 'Rejected'
+            WHEN o.order_status = 4 THEN 'Completed'
+        END as order_status, 
+        o.order_date,
         (oi.list_price * oi.quantity) as revenue,
         (oi.list_price * oi.quantity * oi.discount) as discount_revenue,
-        (revenue - discount_revenue) as net_revenue
+        (revenue - discount_revenue) as net_revenue,
+        CONCAT(c.first_name, ' ', c.last_name) as customer_fullname
         FROM iceberg.warehouse.orders o
         JOIN iceberg.warehouse.order_items oi ON o.order_id = oi.order_id
+        LEFT JOIN iceberg.warehouse.customers c ON o.customer_id = c.customer_id
     """,
     "schema": [
         {"name": "order_id", "type": "BIGINT"}, 
